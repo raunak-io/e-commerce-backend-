6dc78bfbeb84ed8f52eca5fc801ec401e683d586
Raunak.io <raunak.srivastava@nextontop.com>
2021-01-31 22:27:44 +0530

    post img validator bug chkng

--- utils/postImageValidator.js
+++ utils/postImageValidator.js
@@ -1,3 +1,35 @@
+// const multer = require('multer');
+
+// const MIME_TYPE_MAP = {
+//   'image/png': 'png',
+//   'image/jpeg': 'jpg',
+//   'image/jpg': 'jpg'
+// };
+
+// const fileStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     const isValid = MIME_TYPE_MAP[file.mimetype];
+//     let error = new Error('Invalid mime type');
+//     if (isValid) {
+//       error = null;
+//     }
+//     console.log("image validation stage passed")
+//     cb(error, 'images/products');
+//   },
+//   filename: (req, file, cb) => {
+//     const name = file.originalname
+//       .toLowerCase()
+//       .split(' ')
+//       .join('-');
+//     const extension = MIME_TYPE_MAP[file.mimetype];
+//     console.log("image validation stage 111passed",req.body,file)
+//     cb(null, name ); //
+//     console.log("image validation stage passed",req.body,file)
+//   }
+
+// }
+// );
+// module.exports = multer({ storage: fileStorage }).single('image');
 const multer = require('multer');
 
 const MIME_TYPE_MAP = {
@@ -6,6 +38,7 @@ const MIME_TYPE_MAP = {
   'image/jpg': 'jpg'
 };
 
+//cloudinary.uploader.upload(//file path and a call back function)
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     const isValid = MIME_TYPE_MAP[file.mimetype];
@@ -13,7 +46,6 @@ const fileStorage = multer.diskStorage({
     if (isValid) {
       error = null;
     }
-    console.log("image validation stage passed")
     cb(error, 'images/products');
   },
   filename: (req, file, cb) => {
@@ -21,12 +53,7 @@ const fileStorage = multer.diskStorage({
       .toLowerCase()
       .split(' ')
       .join('-');
-    const extension = MIME_TYPE_MAP[file.mimetype];
-    console.log("image validation stage 111passed",req.body,file)
-    cb(null, name ); //
-    console.log("image validation stage passed",req.body,file)
+    cb(null, name);
   }
-
-}
-);
+});
 module.exports = multer({ storage: fileStorage }).single('image');
